9488bbc4d459cffcb9b1958665b32bda0973a48d
Yurii Kazakov <gfnvortex@gmail.com>
2023-11-07 08:40:48 +0200

    project-start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import './App.css';
-
+import MainContainer from './components/MainContainer/MainContainer';
 
 function App() {
     return (
         <>
-
+            <MainContainer/>
         </>
     );
 }

8c48270fed1485e2a8e1e002e240190ee183c454
Yurii Kazakov <gfnvortex@gmail.com>
2023-11-13 16:53:00 +0200

    togle-all

--- src/components/TodoApp/TodoList/TodoList.js
+++ src/components/TodoApp/TodoList/TodoList.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './todo-list.css';
 
-export default function TodoList({todos, onToggleTask, onDeleteTask}) {
+export default function TodoList({ todos, onToggleTask, onDeleteTask }) {
+    const [selectAll, setSelectAll] = useState(false);
+
+    const handleToggleAll = () => {
+        setSelectAll(!selectAll);
+        todos.forEach((todo, index) => onToggleTask(index, !selectAll));
+    };
+
     return (
-        <ul className='todo-list'>
-            {todos.map((todo, index) => (
-                <li
-                    key={index}
-                    onClick={() => onToggleTask(index)}
-                >
-                    <span
-                        className={todo.completed ? 'completed' : ''}
-                        style={{marginRight: '5px'}}>
-                        {todo.text}
-                    </span>
-                    <button
-                        className='delete-button'
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            onDeleteTask(index);
-                        }}
+        <div>
+            <input
+                type="checkbox"
+                checked={selectAll}
+                onChange={handleToggleAll}
+            />
+            <label>Toggle All</label>
+
+            <ul className='todo-list'>
+                {todos.map((todo, index) => (
+                    <li
+                        key={index}
+                        onClick={() => onToggleTask(index)}
+                    >
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={() => onToggleTask(index)}
+                        />
+
+                        <span
+                            className={todo.completed ? 'completed' : ''}
+                            style={{ marginRight: '5px' }}
                         >
-                        -
-                    </button>
-                </li>
-            ))}
-        </ul>
+                            {todo.text}
+                        </span>
+                        <button
+                            className='delete-button'
+                            onClick={(event) => {
+                                event.stopPropagation();
+                                onDeleteTask(index);
+                            }}
+                        >
+                            -
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
-
-
